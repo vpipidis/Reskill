48ae5541dff80c0955a2bebc52e82a867f628294
vpipidis <v.pipidis@gmail.com>
2024-08-28 16:53:29 +0300

    Created Custom Error Page

--- reskill-frontend-app/src/App.jsx
+++ reskill-frontend-app/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import MainLayout from './MainLayout';
 import Post from './components/Post';
 import Posts from './components/Posts';
+import UnderConstraction from './components/UnderConstruction';
 
 
 const router = createBrowserRouter(
@@ -12,6 +13,7 @@ const router = createBrowserRouter(
       <Route index element={<HomePage/>}/>
       <Route path='/post/:id' element={<Post/>}/>
       <Route path='/posts' element={<Posts/>}/>
+      <Route path='/*' element={<UnderConstraction/>}/>
     </Route>
 )
 )
---
+++ reskill-frontend-app/src/components/UnderConstruction.jsx
@@ -0,0 +1,17 @@
+
+
+function UnderConstraction() {
+  
+
+  return (
+    <>
+      <div className=''>  
+    <h1>
+    This Page is Unavailable or Under Construction
+    </h1>
+      </div>
+    </>
+  )
+}
+
+export default UnderConstraction
